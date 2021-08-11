67365b4bbff8d54239c6a9da1d82cebd2b71c78b
Igor Balanici <igor.balanici@gmail.com>
2021-08-11 10:43:16 +0300

    Initial commit.

---
+++ src/main/java/com/example/sandboxrestservice/SandboxRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandboxrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxRestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandboxrestservice/SandboxRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandboxrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
